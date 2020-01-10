b1038a148fcb5a6b1ab16e254d95928e7bfc0984
Ataru Nakano <neu.nakano@gmail.com>
2020-01-10 20:16:50 +0900

    enum

--- www/asset/js/test.js
+++ www/asset/js/test.js
@@ -1,4 +1,7 @@
 "use strict";
+/**
+ * 型指定
+ */
 var num = 100;
 var hello;
 hello = 'こんにちは';
@@ -6,8 +9,27 @@ var yes;
 yes = true;
 var car = 'BMW';
 car = { brand: 'BMW' };
+/**
+ * 配列 - []
+ */
 var hobbies = ['読書', 'スポーツ'];
 hobbies.push('100');
 console.log(hobbies);
+/**
+ * タプル - [x, y]
+ */
 var olympic = ['東京', 2020];
 console.log(olympic);
+/**
+ * 列挙型 - enum
+ */
+// 先頭を大文字
+var Colors;
+(function (Colors) {
+    Colors[Colors["Gray"] = 0] = "Gray";
+    Colors[Colors["Red"] = 1] = "Red";
+    Colors[Colors["Green"] = 100] = "Green";
+    Colors[Colors["Blue"] = 101] = "Blue";
+})(Colors || (Colors = {}));
+var myColor = Colors.Green;
+console.log(myColor);
--- www/asset/js/test.ts
+++ www/asset/js/test.ts
@@ -1,3 +1,6 @@
+/**
+ * 型指定
+ */
 let num: number = 100
 
 let hello: string
@@ -9,9 +12,28 @@ yes = true
 let car: any = 'BMW'
 car = { brand: 'BMW' }
 
+/**
+ * 配列 - []
+ */
 let hobbies: string[] = ['読書', 'スポーツ']
 hobbies.push('100')
 console.log(hobbies)
 
+/**
+ * タプル - [x, y]
+ */
 let olympic: [string, number] = ['東京', 2020]
 console.log(olympic)
+
+/**
+ * 列挙型 - enum
+ */
+// 先頭を大文字
+enum Colors {
+  Gray, // 0
+  Red, // 1
+  Green = 100, // 100
+  Blue, // 101
+}
+let myColor: Colors = Colors.Green
+console.log(myColor)
