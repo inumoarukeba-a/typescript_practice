7f9acf7f07c09f94aef652f6dfbd5d3962b354e1
Ataru Nakano <atarunakano@gmail.com>
2020-01-25 18:20:58 +0900

    tsconfig.json作成

--- www/asset/js/test.ts
+++ www/asset/js/test.ts
@@ -105,3 +105,17 @@ let complex2: Complex = {
     return this.data
   },
 }
+
+/**
+ * union型 - 共同体型、合併型
+ */
+let thisYear: number | string = '2019'
+thisYear = '2019'
+
+function checkLength(str: string | null): number {
+  if (str != null) {
+    return str.length
+  } else {
+    return 0
+  }
+}
