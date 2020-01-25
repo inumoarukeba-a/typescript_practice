247d0956ffa1659f2687122da4eabd2c622c7a5d
Ataru Nakano <a-nakano@n-e-u.co.jp>
2020-01-25 18:20:58 +0900

    tsconfig.json作成

--- www/asset/js/test.js
+++ www/asset/js/test.js
@@ -82,3 +82,16 @@ var complex2 = {
         return this.data;
     },
 };
+/**
+ * union型 - 共同体型、合併型
+ */
+var thisYear = '2019';
+thisYear = '2019';
+function checkLength(str) {
+    if (str != null) {
+        return str.length;
+    }
+    else {
+        return 0;
+    }
+}
